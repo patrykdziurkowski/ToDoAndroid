98516947d1e6453d326b22e7161b2c4ae1ddbecf
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-05-27 15:03:07 +0200

    Replaced findViewById with bindings

--- src/app/src/main/java/com/example/todoandroid/CreateTaskActivity.java
+++ src/app/src/main/java/com/example/todoandroid/CreateTaskActivity.java
@@ -14,33 +14,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.todoandroid.databinding.ActivityCreateTaskBinding;
+import com.example.todoandroid.databinding.FragmentTasksBinding;
+
 import java.util.Calendar;
 
 public class CreateTaskActivity extends AppCompatActivity {
-    private EditText titleEditText;
-    private EditText descriptionEditText;
-    private EditText deadlineEditText;
-    private Button submitButton;
-
-
+    private ActivityCreateTaskBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityCreateTaskBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_task);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        titleEditText = findViewById(R.id.title_editText);
-        descriptionEditText = findViewById(R.id.description_editText);
-        deadlineEditText = findViewById(R.id.deadline_editDate);
-        submitButton = findViewById(R.id.finish_adding_button);
-
-        deadlineEditText.setOnClickListener(new View.OnClickListener() {
+        binding.deadlineEditDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar c = Calendar.getInstance();
@@ -52,7 +47,7 @@ public class CreateTaskActivity extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                                deadlineEditText.setText(String.format("%s-%s-%s", year, month, dayOfMonth));
+                                binding.deadlineEditDate.setText(String.format("%s-%s-%s", year, month, dayOfMonth));
                             }
                         },
                         year, month, day);
@@ -60,14 +55,14 @@ public class CreateTaskActivity extends AppCompatActivity {
             }
         });
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        binding.finishAddingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 Bundle data = new Bundle();
-                data.putString("title", String.valueOf(titleEditText.getText()));
-                data.putString("description", String.valueOf(descriptionEditText.getText()));
-                data.putString("deadline", String.valueOf(deadlineEditText.getText()));
+                data.putString("title", String.valueOf(binding.titleEditText.getText()));
+                data.putString("description", String.valueOf(binding.descriptionEditText.getText()));
+                data.putString("deadline", String.valueOf(binding.deadlineEditDate.getText()));
                 intent.putExtras(data);
                 setResult(RESULT_OK, intent);
                 finish();
